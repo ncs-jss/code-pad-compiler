d876fc981b71cf74e51f548341953fe22bdf33cd
shobhit1997 <shobhit.shobhitagarwal.agarwal@gmail.com>
2018-09-05 01:18:51 +0530

    login routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,30 @@
 const Question= require('../.././app/models/question');
 const User= require('../.././app/models/user');
+const config=require('.././config/config');
 const express=require('express');
+const jwt=require('jsonwebtoken');
 const router=express.Router();
 
+
+authenticate=function(req,res,next){
+	try{
+
+		decoded= jwt.verify(req.header('x-auth'),process.env.JWT_SECRET);
+		if(decoded.username==process.env.USERNAME&&decoded.password==process.env.PASSWORD){
+
+			next();
+		}
+		else{
+			res.status(401).send();	
+		}
+
+	}catch(e){
+		res.status(401).send();
+	}	
+};
+
 router.route('/leaderboard')
-	.get(async function(req,res){
+	.get(authenticate, async function(req,res){
 		var users=await User.find();
 		users.sort(function (a, b) {
 		  return b.questions_solved.length - a.questions_solved.length;
@@ -18,6 +38,21 @@ router.route('/leaderboard')
 			return json;
 		})
 		res.send(users);
+	});
+
+router.route('/login')
+	.post(function(req,res){
+		var username=req.body.username;
+		var password=req.body.password;
+		if(username===process.env.USERNAME && password===process.env.PASSWORD){
+
+			var token = jwt.sign({username,password},process.env.JWT_SECRET).toString();
+			res.header('x-auth',token).send({message:"Successful"});
+
+		}
+		else{
+			res.status(401).send({message:'Invalid Credentials'});
+		}
 	});
 
 module.exports=router;
