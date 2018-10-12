16c17b7f0d90e1705a229b48111a56619d037486
shobhit1997 <shobhit.shobhitagarwal.agarwal@gmail.com>
2018-10-12 14:55:03 +0530

    readme added

---
+++ README.md
@@ -0,0 +1,20 @@
+# Code-pad-compiler
+Codepad Api's .
+
+* install node
+* install mongo
+* run mongod demon service
+* clone repo and cd into it
+
+* run the following commands
+
+    `npm install`
+
+    `npm install -g nodemon`
+    
+* Make the config file according to exampleConfig.json
+
+* after the successfull installation run :
+`nodemon server.js`
+
+wait till cmd logs "Server listening at port 8000"
--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -10,8 +10,7 @@ const client = redis.createClient({host:"redis"});
 authenticate=function(req,res,next){
 	try{
 
-		decoded= jwt.verify(req.header('x-auth'),process.env.JWT_SECRET);
-		if(decoded.username==process.env.USERNAME&&decoded.password==process.env.PASSWORD){
+		if(req.header('client-key')===process){
 
 			next();
 		}
@@ -24,88 +23,62 @@ authenticate=function(req,res,next){
 	}	
 };
 
-router.route('/day/:id')
-
+router.route('/submit')
 /**
- * @api {get} /api/question/day/day_no. Return the questions for the day
+ * @api {post} /api/question/submit Submit solution
  * @apiGroup Questions
- * @apiSuccess {Object[]} questions Question list
- * @apiSuccess {String} questions._id Question id
- * @apiSuccess {String} questions.question Question statement
- * @apiSuccess {String[]} questions.input Question input
- * @apiSuccess {String[]} questions.output Question output
- * @apiSuccess {Number} questions.day Question day
- * @apiSuccessExample {json} Success
- * 	HTTP/1.1 200 OK
-*		[{
-*			"_id":"5b8d2e908a54035801ae7564",
-*			"question":"WAP to input two numbers and print sum.",
-*			"input":["5\n5","5\n4","5\n3"],
-*			"output":["10","9","8"],
-*			"day":1
-*		}]
- * @apiErrorExample {json} Find error
- * 	HTTP/1.1 404 NOT FOUND
-*/
-	.get(function(req,res) {
-		var day=req.params.id;
-		console.log(day);
-		Question.find({day}).then(function(ques){
-			if(ques){
-				res.send(ques);
-			}
-			else{
-				res.status(404).send('Not Found');
-			}
-		}).catch(function(err){
-			console.log(err);
-		});
-	});
-router.route('/add')
-/**
- * @api {post} /api/question/add Add questions
- * @apiGroup Questions
- * @apiParam {String} question Question statement
- * @apiParam {String[]} input Question input
- * @apiParam {String[]} output Question output
- * @apiParam {Number} day Question day
+ * @apiParam {String} code Question solution
+ * @apiParam {String} lang Language used
+ * @apiParam {String} admission_no User Admission No.
+ * @apiParam {String} name User name
+ * @apiParam {String} ques_id Question id
  * @apiParamExample {json} Input
  *		{
- *			"question":"WAP to input two numbers and print sum.",
- *			"input":["5\n5","5\n4","5\n3"],
- *			"output":["10","9","8"],
- *			"day":1
+ *			"code":"#include.........",
+ *			"lang": "c/cpp/java/python",
+ *			"admission_no":"16it028",
+ *			"name":"Shobhit Agarwal",
+ *			"ques_id":"9875fdnkdnfowur9we93"
  *		}
  * @apiHeader {String} Authorization Token of admin
- * @apiHeaderExample {json} Header
- * {"x-auth": "JWT xyz.abc.123.hgf"}
- * @apiSuccess {String} questions._id Question id
- * @apiSuccess {String} questions.question Question statement
- * @apiSuccess {String[]} questions.input Question input
- * @apiSuccess {String[]} questions.output Question output
- * @apiSuccess {String} questions.day Question day
+ * @apiSuccess {String} status Submission Status
+ * @apiSuccess {String} message Solution Status
  * @apiSuccessExample {json} Success
  * 	HTTP/1.1 200 OK
 *		{
-*			"_id":"5b8d2e908a54035801ae7564",
-*			"question":"WAP to input two numbers and print sum.",
-*			"input":["5\n5","5\n4","5\n3"],
-*			"output":["10","9","8"],
-*			"day":1
+*			"status":"success",
+*			"message":"Correct/Wrong"
 *		}
- * @apiErrorExample {json} Find error
- * 	HTTP/1.1 401 Unauthorised
+* @apiErrorExample {json} Find error
+* 	HTTP/1.1 201 ERROR
+*		{
+*			"error":"nfnfnmlem[mswf",
+*			"errorType":"Compile_Time"
+*		} 
 */
-	.post(authenticate, function(req,res){
-		console.log(req.body);
-		var body=_.pick(req.body,['question','day','input','output']);
-		var question=new Question(body);
-		question.save().then(function(ques){
-			res.send(ques);
-		}).catch(function(err){
-			console.log(err);
+	.post( async function(req,res){
+		// console.log(req.body);
+		var admission_no=req.body.admission_no.toUpperCase();
+		var name=req.body.name;
+		var ques_id=req.body.ques_id;
+		var user=await User.findOne({admission_no});
+		if(!user){
+			user=new User({admission_no,name});
+		}
+		client.get(ques_id,async function(err,result){
+			console.log(result);
+			if(result){
+				compileCode(req,res,user,result);
+			}
+			else{
+				var ques=await Question.findById(ques_id);
+				compileCode(req,res,user,ques);
+			}
 		});
+				
+
 	});
+
 router.route('/submit')
 /**
  * @api {post} /api/question/submit Submit solution
